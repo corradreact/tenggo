e7bd046837c7f2bb719a7bb319711542d958c2b2
Syahril Sulaiman <syahrilrevolusi@gmail.com>
2023-05-29 10:26:40 +0800

    teng commit - commit last changes

--- pages/api/bill/create.js
+++ pages/api/bill/create.js
@@ -1,11 +1,13 @@
 export default function handler(req, res) {
+
+    const jwt = require("json5")
     res
     .status(200)
     .setHeader('Access-Control-Allow-Origin', '*')
     .json(
         { 
             name: 'Saleha Kema',
-            username: process.env.DB_USERNAME
+            username: jwt.stringify("jahskdjhakjshdkajhsd")
         }
     );
 }
